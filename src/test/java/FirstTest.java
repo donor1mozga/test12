dd4fa1559d1216dfedac4af10a0cae166e466148
donor1mozga <donor1mozga@gmail.com>
2023-11-26 21:11:11 +0200

    t

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,10 +1,15 @@
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+
 public class FirstTest {
 
 
     @Test
     public void main() {
         System.out.println("Start test");
+        open("https://rozetka.com.ua/");
+        sleep(5000);
     }
 }
--- src/test/java/SecondTest.java
+++ src/test/java/SecondTest.java
@@ -1,9 +1,14 @@
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+
 public class SecondTest {
 
     @Test
     public void mainTwo() {
         System.out.println("Finished test");
+        open("https://www.ctrs.com.ua/");
+        sleep(5000);
     }
 }
